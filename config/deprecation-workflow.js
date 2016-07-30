868126dbaae495f351620b468467b87c1e308da7
Tomasz Subik <tsubik@gmail.com>
2016-07-30 16:56:44 +0200

    trying ember alpha with glimmer2

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,5 +2,7 @@ window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
     workflow: [
         { handler: "silence", matchId: "ember-application.injected-container" },
+        { handler: "silence", matchId: "ember-htmlbars.ember-handlebars-safestring" },
+        { handler: "silence", matchId: "ember-runtime.enumerable-contains" },
     ],
 };
